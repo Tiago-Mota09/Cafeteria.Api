d5bbdb56ee72962918e795daa743a01339a24c84
Tiago Mota <tiagodiggermota@hotmail.com>
2021-06-25 14:11:36 -0300

    Feat: Criando camadas

---
+++ Cafeteria.Api/Domain/Models/CafeResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafeteria.Api.Domain.Models
+{
+    public class CafeResponse
+    {
+        public int IdCafe { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Ingredientes { get; set; }
+        public char Tamanho { get; set; }
+        public double Preco { get; set; } 
+    }
+}
