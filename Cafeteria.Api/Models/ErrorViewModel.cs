8ab43149c61c2b5e0ed5b94b81a9222e23e41613
Tiago Mota <tiagodiggermota@hotmail.com>
2021-06-25 12:13:42 -0300

    Feat: Criando Projeto Cafeteria

---
+++ Cafeteria.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cafeteria.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
